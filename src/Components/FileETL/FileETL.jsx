7d55de942fd4fb69ad4db80fc82942fa46d2ce53
Rob Smith <smith.rob@student.greenriver.edu>
2025-01-19 16:57:17 -0800

    adding fileETL component

---
+++ src/Components/FileETL/FileETL.jsx
@@ -0,0 +1,9 @@
+function FileETL() {
+  return (
+    <div>
+      <h1>File ETL</h1>
+    </div>
+  );
+}
+
+export default FileETL;
--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./Components/App/App.jsx";
 import TitleBar from "./Components/TitleBar/TitleBar.jsx";
+import FileETL from "./Components/FileETL/FileETL.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <TitleBar />
+    <FileETL />
     <App />
   </React.StrictMode>
 );
